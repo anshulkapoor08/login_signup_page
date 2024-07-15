cdf4d99f62d62a3ff900ead1bd341cc2ab043b18
Anshul Kapoor <anshulkapoor306@gmail.com>
2024-07-16 00:13:12 +0530

    login and signup using APIs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:login_signup/login.dart';
 import 'package:login_signup/signup.dart';
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,18 +1,45 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:login_signup/login.dart';
 
 class Myregister extends StatefulWidget {
-  const Myregister({super.key});
+  const Myregister({Key? key}) : super(key: key);
 
   @override
   State<Myregister> createState() => _MyregisterState();
 }
 
 class _MyregisterState extends State<Myregister> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  void signup(String email, String password) async {
+    try {
+      http.Response response = await http.post(
+        Uri.parse('https://reqres.in/api/register'),
+        body: {
+          'email': email,
+          'password': password,
+        },
+      );
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body.toString());
+        print(data);
+        print('Success');
+      } else {
+        print('Failed');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         image: DecorationImage(
           image: AssetImage('lib/images/register.png'),
           fit: BoxFit.cover,
@@ -27,11 +54,11 @@ class _MyregisterState extends State<Myregister> {
         body: Stack(
           children: [
             Container(
-              margin: EdgeInsets.only(
+              margin: const EdgeInsets.only(
                 left: 35,
                 top: 40,
               ),
-              child: Text(
+              child: const Text(
                 'Create \nAccount',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
@@ -49,21 +76,23 @@ class _MyregisterState extends State<Myregister> {
                 ),
                 child: Column(
                   children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        fillColor: Colors.grey.shade300,
-                        filled: true,
-                        hintText: 'Name',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                     TextField(
+
+                        decoration: InputDecoration(
+                          fillColor: Colors.grey.shade300,
+                          filled: true,
+                          hintText: 'Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
+                      
+                    const SizedBox(
                       height: 40,
                     ),
                     TextField(
-                      // obscureText: true,
+                      controller: emailController,
                       decoration: InputDecoration(
                           fillColor: Colors.grey.shade300,
                           filled: true,
@@ -71,10 +100,11 @@ class _MyregisterState extends State<Myregister> {
                           border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10))),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 40,
                     ),
                     TextField(
+                      controller: passwordController,
                       obscureText: true,
                       decoration: InputDecoration(
                         fillColor: Colors.grey.shade300,
@@ -85,7 +115,7 @@ class _MyregisterState extends State<Myregister> {
                         ),
                       ),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 60,
                     ),
                     Row(
@@ -98,31 +128,34 @@ class _MyregisterState extends State<Myregister> {
                               color: Colors.indigo.shade500,
                               borderRadius: BorderRadius.circular(10)),
                           child: InkWell(
-                            onTap: () {},
+                            onTap: () {
+                              signup(
+                                emailController.text.toString(),
+                                passwordController.text.toString(),
+                              );
+                            },
                             child: Container(
-                              padding: EdgeInsets.only(
+                              padding: const EdgeInsets.only(
                                   left: 75, top: 13, bottom: 14),
-                              child: Text(
+                              child: const Text(
                                 'CREATE ACCOUNT',
                                 style: TextStyle(
-                                    // decoration: TextDecoration.underline,
-                                    color: Colors.white,
-                                    fontSize: 18),
+                                    color: Colors.white, fontSize: 18),
                               ),
                             ),
                           ),
                         ),
                       ],
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 25,
                     ),
                     Row(
                       children: [
                         Container(
-                          margin: EdgeInsets.only(left: 55),
-                          child: Text(
-                            'Already have a account?',
+                          margin: const EdgeInsets.only(left: 55),
+                          child: const Text(
+                            'Already have an account?',
                             style: TextStyle(
                               fontSize: 15,
                               color: Colors.black54,
@@ -132,13 +165,15 @@ class _MyregisterState extends State<Myregister> {
                         Container(
                           child: InkWell(
                             onTap: () {
-                              Navigator.push(context, MaterialPageRoute(builder: (context)=> Mylogin()));
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Mylogin()));
                             },
-                            child: Text(
+                            child: const Text(
                               'Login',
                               style: TextStyle(
                                 fontSize: 15,
-                                
                                 color: Colors.indigo,
                                 decoration: TextDecoration.underline,
                               ),
